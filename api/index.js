201e3b4d772dcc795b64b4000151cb77162ec622
Premal <goudarpremal@gmail.com>
2025-05-07 18:27:52 +0530

    change

--- api/index.js
+++ api/index.js
@@ -12,7 +12,8 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:5173'
+    // origin: 'http://localhost:5173'
+    origin: 'https://task-management-yylu.onrender.com/api/task'
 }))
 
 
